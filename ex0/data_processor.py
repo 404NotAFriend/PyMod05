02ec44353b64aa1105e7a8a705400af3a48eecc6
404NotAFriend <brunodrcgomes@gmail.com>
2026-06-03 23:16:07 +0100

    started Module05 , created ex0 folder , touch data_processor.py and chmod +x to it , created imports , base classes with abstractmethods

---
+++ ex0/data_processor.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+from abc import ABC, abstractmethod
+from typing import Any
+
+
+class  DataProcessor(ABC):
+    @abstractmethod
+    def validate(self, data: Any) -> bool:
+        pass
+    @abstractmethod
+    def ingest(self, data: Any) -> None:
+        pass
+    def output(self) -> tuple[int, str]:
+        pass
+
+
+class NumericProcessor(DataProcessor):
+
+
+class TextProcessor(DataProcessor):
+
+
+class LogProcessor(DataProcessor):
